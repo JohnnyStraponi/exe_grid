3d725e503965279299e27db93f8d03df229fe592
JohnnyStraponi <45066708+JohnnyStraponi@users.noreply.github.com>
2020-07-02 13:51:54 +0300

    Clients section added
    - update index.html
    - create clients.scss
    - add owl.carousel.js
    - add carousel.scss

--- js/script.js
+++ js/script.js
@@ -41,13 +41,16 @@ window.addEventListener('DOMContentLoaded', () => {
   //Animation
 
   const progressLine = document.querySelectorAll('.progress__line'),
-        progressTooltip = document.querySelectorAll('.progress__tooltip');
+    progressTooltip = document.querySelectorAll('.progress__tooltip');
 
   function moveProgressBar() {
     progressLine.forEach(item => {
-      item.animate([
-        {width: 0 + '%'},
-        {width: item.firstElementChild.textContent}
+      item.animate([{
+          width: 0 + '%'
+        },
+        {
+          width: item.firstElementChild.textContent
+        }
       ], 1500);
       item.style.width = item.firstElementChild.textContent;
     });
@@ -60,6 +63,31 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   moveProgressBar();
+
+  //Carousel
+
+  $('.carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    dots: false,
+    navText: [],
+    items: 5,
+    responsive: {
+      0: {
+        items: 1
+      },
+      480: {
+        items: 2
+      },
+      768: {
+        items: 3
+      },
+      980: {
+        items: 5
+      }
+    }
+  })
 
   //
 });
